1dcea1ba884b5d1974f0a773e3a889bedcae0b59
Khalel <KAbaquin@west-mec.org>
2019-08-19 14:15:31 -0700

    Update 1
    Completed Const & Let

---
+++ sub-unit-1/02_let_const/app.js
@@ -0,0 +1,22 @@
+"use-strict"
+
+var a = "Test1";
+let b = "Test2";
+
+const testVar = (params) => {
+    const a = 30;
+    if(true) {
+        let a = 50;
+        console.log(a);
+    }
+    console.log(`other A: ${a}`);
+}
+
+console.log(`_Logged: ${a}`);
+
+testVar();
+
+// const _list = [0,1,2,3];
+// const _items = [4,5,6];
+// _list.push(_items);
+// console.log(`_Logged ${_list}`);
